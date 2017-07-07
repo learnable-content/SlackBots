e722f99d8995e734ba0727bde46598a2535aa6cd
melissa <mactechy@gmail.com>
2017-07-06 20:58:28 -0400

    updated files

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# SlackBots
+# Outgoing Webhooks vs Slash Commands
+
+This Slackbot shows how to use outgoing webhooks and Slash Commands with BotKit. It belongs to the fourth part of a series of tutorials about Slackbots.
+
+You can follow the [tutorial](http://sitepoint.com) to build this application or jump straight to the code.
+
+# Requirements
+- A Slack account with administrative privileges.
+- A Wit.ai account.
+- [Node.js](https://nodejs.org/en/download/) version 5 or higher.
+
+# Installation
+1. Clone this branch and `cd` into it.
+2. Open a terminal window and execute the following command to create a config file: `cp config.sample.js config.js`.
+3. Go to https://my.slack.com/apps/build/custom-integration and create a Bot.
+4. Return to https://my.slack.com/apps/build/custom-integration and this time, create an Incoming Webhook.
+5. Copy the Webhook URL into `config.js`. While you're there, you can also change the frequency in which the the incoming webhooks will be sent.
+6. Open a new terminal window and install [Localtunnel](https://localtunnel.github.io/www/) globally with `npm install -g localtunnel`.
+7. Once installed, execute `lt --port 3000 --subdomain testingbots` (or any other subdomain if it's not available).
+8. Return to https://my.slack.com/apps/build/custom-integration and this time, create an Outgoing Webhook. In the URL enter `https://testingbots.localtunnel.me/slack/receive` (or any other subdomain you specified in the previous step).
+9. Return to https://my.slack.com/apps/build/custom-integration and this time, create an Slash Command. In the URL enter `https://testingbots.localtunnel.me/slack/receive` (or any other subdomain you specified in the previous step).
+10. In another window, go to https://wit.ai/ and create a new app by importing `NumberBotApp.zip`.
+11. Once the app is created, wait a few seconds and check if the circle next to the app name becomes green. As Wit.ai is still in beta, sometimes this doesn't happen, so go to the Settings tab to delete the app and create it again. You may have to do this a few times before the app shows a green status.
+12. Once the app is created correctly, go to the Settings tab and generate a Server Access Token if none is shown by clicking the *Reset Token* icon.
+13. In a terminal window, install the app dependencies with `npm install`
+14. Start the bot with the command `token=<YOUR_BOT_TOKEN> wit_token=<YOUR_WIT_AI_SERVER_ACCESS_TOKEN> ow_token=<OUTGOING_WEBHOOK_TOKEN> cmd_token=<SLASH_COMMAND_TOKEN> node bot.js` by copying the token given by Slack (when you created the bot), the Wit.ai token, the Outgoing Webhook token, and the Slash Command token.
+15. Play with the bot by sending a message with the keyword configured to trigger the Outgoing Webhook and by executing the configured Slash Command.
+
+# License
+MIT
---
+++ bot.js
@@ -0,0 +1,257 @@
+const Botkit = require('botkit');
+const request = require('superagent');
+const Wit = require('node-wit').Wit;
+const Log = require('node-wit').log;
+const config = require('./config');
+
+// Cheking for the Slack, Wit.ai, Outgoing Webhook token, and Slack Command tokens
+if (!process.env.token || !process.env.wit_token
+    || !process.env.ow_token || !process.env.cmd_token) {
+  console.log('Error: Specify a Slack token, a Wit token, ' +
+    'Outgoing Webhook token, and Slash Command token as environment variables');
+  process.exit(1);
+}
+
+// Server port for outcoming webhook and slack command
+const port = process.env.port || 3000;
+
+// Creates the Slack bot
+const controller = Botkit.slackbot();
+
+// Starts the websocket connection with the incoming webhook configuration
+const botObj = controller.spawn({
+  token: process.env.token,
+  incoming_webhook: {
+    url: config.WEBHOOK_URL,
+  },
+}).startRTM((err) => {
+  if (err) {
+    console.error(`Error: Could not start the bot - ${err}`);
+  }
+});
+
+// Outcoming webhook and slack command specific code
+controller.setupWebserver(port, (err, expressWebserver) => {
+  controller.createWebhookEndpoints(expressWebserver,
+    [process.env.ow_token, process.env.cmd_token]
+  );
+});
+
+controller.on('outgoing_webhook', (bot, message) => {
+  console.log(message);
+
+  // reply to outgoing webhook command
+  bot.replyPublic(message, 'Everyone can see the result of this webhook');
+});
+
+controller.on('slash_command', (bot, message) => {
+  console.log(message);
+
+  let number;
+
+  if (message.text !== '') {
+    number = message.text;
+  } else {
+    number = Math.floor(Math.random() * 100);
+  }
+
+  // Immediately reply a confirmation to user
+  bot.replyPrivate(message, 'Command received...');
+
+  request
+    .get(`http://numbersapi.com/${number}`)
+    .end((err, res) => {
+      if (err) {
+        bot.replyPrivateDelayed(message, 'Got an error, can you try again with a valid number?');
+      } else {
+        bot.replyPrivateDelayed(message, res.text);
+      }
+    }
+  );
+});
+
+
+// Incoming webhook specific code
+const sendTrivia = () => {
+  const date = new Date();
+  const today = `${date.getMonth() + 1}/${date.getDate()}`;
+
+  request
+    .get(`http://numbersapi.com/${today}/date`)
+    .end((err, res) => {
+      if (err) {
+        console.error('Got an error from the Numbers API: ', err.stack || err);
+      } else {
+        // Send the webhook
+        botObj.sendWebhook({
+          text: res.text,
+        },
+        (webhookErr, webhookRes) => {
+          if (webhookErr) {
+            console.error('Got an error when sending the webhook: ', webhookErr.stack || webhookErr);
+          } else {
+            console.log(webhookRes);
+          }
+        });
+      }
+    }
+  );
+};
+
+// Send an incoming webhook every X milliseconds
+const interval = config.SEND_TRIVIA_FREQ_MS;
+setInterval(sendTrivia, interval);
+
+
+// Listening for the event when the bot joins a channel
+controller.on('channel_joined', (bot, { channel: { id, name } }) => {
+  bot.say({
+    text: `Thank you for inviting me to channel ${name}`,
+    channel: id,
+  });
+});
+
+// When someone reference a number in a message
+controller.hears(['[0-9]+'], ['ambient'], (bot, message) => {
+  const [number] = message.match;
+  request
+    .get(`http://numbersapi.com/${number}`)
+    .end((err, res) => {
+      if (!err) {
+        bot.reply(message, res.text);
+      }
+    });
+});
+
+// Wit.ai bot specific code
+
+// This will contain all user sessions.
+// Each session has an entry:
+// sessionId -> {userId, context: sessionState, botObject, messageObject}
+const sessions = {};
+
+const maybeCreateSession = (userId, bot, message) => {
+  if (!sessions[userId]) {
+    // No session found for user, let's create a new one
+    sessions[userId] = {
+      userId,
+      context: {},
+      bot,
+      message,
+    };
+  }
+
+  return userId;
+};
+
+// Extract an entity value from the entities returned by Wit
+const firstEntityValue = (entities, entity) => {
+  const match = entities && entities[entity];
+  const isFullArray = Array.isArray(match) && match.length > 0;
+  const val = isFullArray ? match[0].value : null;
+
+  if (!val) {
+    return null;
+  }
+
+  return typeof val === 'object' ? val.value : val;
+};
+
+// Our bot actions
+const actions = {
+  send(req, res) {
+    // Our bot has something to say!
+    // Let's retrieve the bot and message objects to post a message
+    const { bot, message } = sessions[req.sessionId];
+    const text = res.text;
+
+    // We return a promise to let our bot know when we're done sending
+    return new Promise((resolve) => {
+      bot.reply(message, text);
+      return resolve();
+    });
+  },
+  getTrivia({ context, entities }) {
+    return new Promise((resolve) => {
+      const intent = firstEntityValue(entities, 'intent');
+      const rawType = firstEntityValue(entities, 'type');
+      const random = firstEntityValue(entities, 'random');
+
+      const type = (rawType
+          ? rawType !== 'general' ? rawType : ''
+          : context.type)
+        || '';
+      const number = random ? 'random' : firstEntityValue(entities, 'number');
+      const newContext = Object.assign({}, context);
+
+      if ((intent && intent === 'trivia') || number) {
+        if (number) {
+          // Make the request to the API
+          request
+            .get(`http://numbersapi.com/${number}/${type}`)
+            .end((err, { text }) => {
+              if (err) {
+                newContext.response = 'Sorry, I couldn\'t process your request';
+              } else {
+                newContext.response = text;
+              }
+              newContext.done = true;
+              delete newContext.missingNumber;
+
+              return resolve(newContext);
+            }
+          );
+        } else {
+          newContext.type = type;
+          newContext.missingNumber = true;
+
+          return resolve(newContext);
+        }
+      } else {
+        newContext.response = 'Sorry, I didn\'t understand what you wanted. I\'m still just a bot, ' +
+          'can you try again?';
+        newContext.done = true;
+
+        return resolve(newContext);
+      }
+    });
+  },
+};
+
+// Setting up our bot
+const wit = new Wit({
+  accessToken: process.env.wit_token,
+  actions,
+  logger: new Log.Logger(Log.INFO),
+});
+
+controller.hears(['(.*)'], ['direct_mention', 'mention'], (bot, message) => {
+  const [text] = message.match;
+
+  // We retrieve the user's current session, or create one if it doesn't exist
+  // This is needed for our bot to figure out the conversation history
+  const sessionId = maybeCreateSession(message.user, bot, message);
+
+  // Let's forward the message to the Wit.ai Bot Engine
+  // This will run all actions until our bot has nothing left to do
+  wit.runActions(
+    sessionId, // the user's current session
+    text, // the user's message
+    sessions[sessionId].context // the user's current session state
+  ).then((context) => {
+    // Our bot did everything it has to do.
+    // Now it will be waiting for further user messages to proceed.
+
+    // Based on the session state, you might want to reset or update the session.
+    if (context.done) {
+      delete sessions[sessionId];
+    } else {
+      // Updating the user's current session state
+      sessions[sessionId].context = context;
+    }
+  })
+    .catch((err) => {
+      console.error('Got an error from Wit: ', err.stack || err);
+    });
+});
+
